0404906ef4f167cdb1c33d8a1e0387bb99a598af
pickelbite3 <yodelingpickle79@gmail.com>
2025-05-02 19:01:54 -0700

    better sign in/up pages

--- site/signup/signup.js
+++ site/signup/signup.js
@@ -5,3 +5,27 @@
 // check database and check email usage (taken/not taken), report new user to database
 
 // sha256 password encryption
+
+
+const pwdMatchMarker = document.getElementById('pwd-check');
+const passwordInput = document.getElementById('password-in');
+const passwordCheck = document.getElementById('password-conf-in');
+
+function checkPassword() {
+    const pwd = passwordInput.value;
+    const pwdCheck = passwordCheck.value;
+
+    if ((pwd && pwdCheck && pwd === pwdCheck) && (pwd.length >= 5)) {
+        pwdMatchMarker.classList.remove('fa-xmark');
+        pwdMatchMarker.classList.add('fa-check');
+        pwdMatchMarker.style.color = 'green';
+    } else {
+        pwdMatchMarker.classList.remove('fa-check');
+        pwdMatchMarker.classList.add('fa-xmark');
+        pwdMatchMarker.style.color = 'red';
+    }
+}
+
+// Add event listeners for real-time checking
+passwordInput.addEventListener('input', checkPassword);
+passwordCheck.addEventListener('input', checkPassword);
